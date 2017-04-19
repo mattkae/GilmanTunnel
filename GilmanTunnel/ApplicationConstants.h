85d15592eb4949dc368f82ff277eff9d27acd1aa
mattkae <mattjaekae@gmail.com>
2017-04-18 20:04:23 -0400

    Crossing perfectly

--- GilmanTunnel/ApplicationConstants.h
+++ GilmanTunnel/ApplicationConstants.h
@@ -13,6 +13,11 @@ namespace ApplicationConstants {
 	const float TargetFps_		= 60.f;
 	const float OptimalTime_	= Second_ / TargetFps_;
 
-	const unsigned int DefaultWidth_ = 640;
-	const unsigned int DefaultHeight_ = 480;
+	const unsigned int DefaultWidth_ = 1280;
+	const unsigned int DefaultHeight_ = 960;
+
+	const unsigned int DepthWidth_ = 640;
+	const unsigned int DepthHeight_ = 480;
+
+	const USHORT MaxDepth_ = 1700.f;
 }
